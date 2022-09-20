ebbfe13b07afb985110fdac6e29a8ecf1b4e69fb
Aleksey Terzi <aleksey.terzi@gmail.com>
2022-09-19 19:07:23 -0600

    Remove CivGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,38 @@
-import net.civmc.civgradle.common.util.civRepo
-
 plugins {
 	`java-library`
 	`maven-publish`
-	id("net.civmc.civgradle.plugin") version "1.0.0-SNAPSHOT"
+}
+
+gradle.buildFinished {
+	project.buildDir.deleteRecursively()
 }
 
 allprojects {
-	group = "net.civmc.namecolors"
-	version = "2.0.0-SNAPSHOT"
-	description = "NameColors"
+	group = rootProject.group
+	version = rootProject.version
+	description = rootProject.description
 }
 
 subprojects {
-	apply(plugin = "net.civmc.civgradle.plugin")
 	apply(plugin = "java-library")
 	apply(plugin = "maven-publish")
 
 	java {
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of(17))
+		toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+		withJavadocJar()
+		withSourcesJar()
+	}
+
+	tasks {
+		compileJava {
+			options.encoding = Charsets.UTF_8.name()
+			options.release.set(17)
+		}
+		processResources {
+			filteringCharset = Charsets.UTF_8.name()
+			filesMatching("**/plugin.yml") {
+				expand( project.properties )
+			}
 		}
 	}
 
@@ -30,24 +43,21 @@ subprojects {
 		maven("https://repo.codemc.io/repository/maven-public/")
 		maven("https://repo.kryptonmc.org/releases")
 
-		civRepo("CivMC/CivModCore")
-		civRepo("CivMC/NameLayer")
-		civRepo("CivMC/CivChat2")
+		maven("https://nexus.civunion.com/repository/maven-public/")
 	}
 
 	publishing {
 		repositories {
 			maven {
-				name = "GitHubPackages"
-				url = uri("https://maven.pkg.github.com/CivMC/NameColors")
+				url = uri("https://nexus.civunion.com/repository/maven-releases/")
 				credentials {
-					username = System.getenv("GITHUB_ACTOR")
-					password = System.getenv("GITHUB_TOKEN")
+					username = System.getenv("REPO_USERNAME")
+					password = System.getenv("REPO_PASSWORD")
 				}
 			}
 		}
 		publications {
-			register<MavenPublication>("gpr") {
+			register<MavenPublication>("main") {
 				from(components["java"])
 			}
 		}
--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,20 +1,13 @@
 plugins {
     `java-library`
-    id("net.civmc.civgradle.plugin")
-    id("io.papermc.paperweight.userdev") version "1.3.1"
-}
-
-civGradle {
-    paper {
-        pluginName = "NameColors"
-    }
+    id("io.papermc.paperweight.userdev") version "1.3.8"
 }
 
 dependencies {
     paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-    compileOnly("net.civmc:civmodcore:2.0.0-SNAPSHOT:dev-all")
-    compileOnly("net.civmc:namelayer-spigot:3.0.0-SNAPSHOT:dev")
-    compileOnly("net.civmc:civchat2:2.0.0-SNAPSHOT:dev")
+    compileOnly("net.civmc.civmodcore:CivModCore:2.4.0:dev-all")
+    compileOnly("net.civmc.namelayer:NameLayer:3.1.0:dev")
+    compileOnly("net.civmc.civchat2:CivChat2:2.1.0:dev")
     compileOnly("me.neznamy:tab-api:3.0.2")
 }
